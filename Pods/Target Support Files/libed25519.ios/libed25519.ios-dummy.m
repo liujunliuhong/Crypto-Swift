e8cdc4356561e1b01869f96dbe621fb52683e2b8
liujun <1035841713@qq.com>
2021-08-30 16:44:59 +0800

    rm cached

--- Pods/Target Support Files/libed25519.ios/libed25519.ios-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_libed25519_ios : NSObject
-@end
-@implementation PodsDummy_libed25519_ios
-@end
--- Pods/Target Support Files/libsecp256k1.ios/libsecp256k1.ios-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_libsecp256k1_ios : NSObject
-@end
-@implementation PodsDummy_libsecp256k1_ios
-@end
